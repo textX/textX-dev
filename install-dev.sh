9b5cfaf2dd2ff3e5e0edeac2e15f23c62218fc5a
Igor Dejanovic <igor.dejanovic@gmail.com>
2025-05-07 07:24:06 +0200

    setup: fix install dev script

--- install-dev.sh
+++ install-dev.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
 pip install --upgrade pip || exit 1
-pip install -e .[dev,test] || exit 1
-pip install -e tests/cli/subcommands/example_project || exit 1
-pip install -e tests/cli/projects/types_dsl || exit 1
-pip install -e tests/cli/projects/data_dsl || exit 1
-pip install -e tests/cli/projects/flow_dsl || exit 1
-pip install -e tests/cli/projects/flow_codegen || exit 1
+pip install -e .[dev] || exit 1
